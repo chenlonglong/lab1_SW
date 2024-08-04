21d430273bd8f49af6855ec5ea53316e0276d957
chenlonglong <e5541135@gmail.com>
2024-08-05 00:34:08 +0800

    first commit

---
+++ src/tb_verilog/INCA_libs/irun.lnx8664.15.20.nc/.timestamp.ts
@@ -0,0 +1,3 @@
+1678427059 /home/chenlonglong/NTU/NTUEE-DCLAB-Materials/lab_bouns_SW/src/tb_verilog/SW_define.v
+1678427059 /home/chenlonglong/NTU/NTUEE-DCLAB-Materials/lab_bouns_SW/src/tb_verilog/tb.sv
+1722787952 /home/chenlonglong/NTU/NTUEE-DCLAB-Materials/lab_bouns_SW/src/SW_Core.sv
